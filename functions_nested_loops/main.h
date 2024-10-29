fa3a29ec5d22f3c3a44a4a1357cd4385167295ca
realtalla25 <tallasamb2525@gmail.com>
2024-10-29 14:15:13 +0000

    task05

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototype for print_sign */
+int print_sign(int n);
+
+/* Function prototype for _putchar */
 int _putchar(char c);
-int _isalpha(int c);
 
 #endif /* MAIN_H */
 
