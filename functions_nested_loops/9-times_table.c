5826ef6da31735691fb0a3eb9e94df165c269e9d
realtalla25 <tallasamb2525@gmail.com>
2024-10-30 14:47:52 +0000

    task09

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,29 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
-/**
- * times_table - prints the 9 times table
- */
 void times_table(void)
 {
 	int i, j, product;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			product = i * j;
-			if (j == 0)
+			if (j != 0)
 			{
-				/* First number in the row, print without leading space */
-				printf("%d", product);
+				printf(", ");
 			}
-			else
-			{
-				/* For subsequent numbers, print a comma and space */
-				printf(", %d", product);
-			}
+			printf("%d", product);
 		}
-		/* Newline after each row */
 		printf("\n");
 	}
 }
